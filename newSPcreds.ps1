affcc570c9e91e9db6cafa789d3d8d0dbfafe8c4
Julie McGlensey <35898049+jam9658@users.noreply.github.com>
2021-03-15 11:32:45 -0400

    Add files via upload

---
+++ newSPcreds.ps1
@@ -0,0 +1,14 @@
+﻿# author - Julie McGlensey
+# this script first creates a new self-signed certificate,
+# then adds the new certificate to an existing service principal
+
+$SPname = "changeMeToSPName"
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" `
+  -Subject "CN=testCert" `
+  -KeySpec KeyExchange
+
+Get-AzADApplication -DisplayName $SPname | New-AzADAppCredential `
+  -CertValue $keyValue `
+  -EndDate $cert.NotAfter `
+  -StartDate $cert.NotBefore
