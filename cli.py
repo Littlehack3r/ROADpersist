0f1d1618251b39b73295be6cf1b6490b48c0a977
LittleHack3r <sxm6549@rit.edu>
2021-03-20 13:32:33 -0400

    subparsing works

--- cli.py
+++ cli.py
@@ -16,14 +16,18 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(description='ROADpersist automates small actions in AzureAD that create persistence')
-group = parser.add_mutually_exclusive_group(required=True) # one action at a time
+subparser = parser.add_subparsers(dest='command')
+addUser = subparser.add_parser('addUser', help='AddUser required flags -Password -DisplayName -NickName -PrincipalName')
+test = subparser.add_parser('test')
 
 # AddUser -Password -DisplayName -NickName -PrincipalName 
 # Try to take first DisplayName and make it the PrincipalName @ email we pull from database 
 # See if we can copy naming conventino of envuronment automatically  
 
-group.add_argument('-AddUser', '-au', dest='addUser', action="store_true", help='AddUser required flags -Password -DisplayName -NickName -PrincipalName')
-
+addUser.add_argument('--Password', type=str, required=True)
+addUser.add_argument('--DisplayName', type=str, required=True)
+addUser.add_argument('--PrincipalName', type=str, required=True)
+addUser.add_argument('--NickName', type=str, required=True)
 
 args = parser.parse_args() 
 
@@ -31,5 +35,11 @@ args = parser.parse_args()
 # Example:
     # if arg.adduser & arg.Display name & NickName & PrincipalName & Password then run ELSE run error message 
 
-if args.AddUser and (args.password is None or args.NickName is None args.displayname is None or args.PrincipalName is None):
-    parser.error("--AddUser requires -Password -DisplayName -NickName -PrincipalName.")
+#if args.AddUser and (args.password is None or args.NickName is None args.displayname is None or args.PrincipalName is None):
+#    parser.error("--AddUser requires -Password -DisplayName -NickName -PrincipalName.")
+
+if args.command == 'addUser':
+    print("Run Script with args:", args.Password, args.NickName)
+
+elif args.command == 'test':
+    print("other option")
