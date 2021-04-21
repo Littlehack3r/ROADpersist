f527e59e1acedf164b42195703851a6b8024c140
Julie McGlensey <35898049+jam9658@users.noreply.github.com>
2021-04-21 16:36:53 -0400

    Update newSPcreds.ps1

--- newSPcreds.ps1
+++ newSPcreds.ps1
@@ -1,12 +1,15 @@
 ﻿# author - Julie McGlensey
 # this script adds a new certificate to an existing service principal
 
+$spName = Read-Host -Prompt 'Input the service principal display name to add credentials to'
+$certName = Read-Host -Prompt 'Input the certificate name you want to be added'
+
 $cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" `
-  -Subject "CN=certificateNameGoesHere" `
+  -Subject "CN=${certName}" `
   -KeySpec KeyExchange
 $keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
 
-Get-AzADServicePrincipal -DisplayName servicePrincipalNameGoesHere | New-AzADSpCredential `
+Get-AzADServicePrincipal -DisplayName $spName | New-AzADSpCredential `
   -CertValue $keyValue `
   -EndDate $cert.NotAfter `
   -StartDate $cert.NotBefore
